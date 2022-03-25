6c16988550d3b44baf878604a232cfb1156dfc27
Kevin X <tnkgavas363@gmail.com>
2022-03-24 19:44:11 -0700

    Added a function singleLog that takes a single element with the ID 'side-nav', and then logs it to the console.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+function singleLog(){
+    const logElement = document.querySelector("#side-nav");         // Selects the section element with the ID "side-nav".
+    console.log(logElement);                                        // Logs the single selected element.
+}
+
 function darkmode(){
-    const dark = document.querySelector(".container");
-    dark.classList.replace("light-theme", 'dark-theme');
+    const dark = document.querySelector(".container");              // Selects the element with the class "container".
+    dark.classList.replace("light-theme", 'dark-theme');            // Replaces "light-theme" with "dark-theme".
 }
 
 function lightmode(){
-    const light = document.querySelector(".container");
-    light.classList.replace("dark-theme", "light-theme");
+    const light = document.querySelector(".container");             // Selects the element with the class "container".
+    light.classList.replace("dark-theme", "light-theme");           // Replaces "dark-theme" with "light-theme".
 }
